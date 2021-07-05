10740a228727c4a4392ea75d128ff59118a40100
Xiaosen9807 <85266718+Xiaosen9807@users.noreply.github.com>
2021-07-05 21:28:07 +0800

    Update Interpolation.py

--- Interpolation.py
+++ Interpolation.py
@@ -6,6 +6,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import plotly.express as px
 
 from scipy import interpolate
 
@@ -60,15 +61,57 @@ for s in range(12):
     t += 1
 
 Table = pd.DataFrame({'LOAD': RESLOAD_cell, 'P': P, 'U': RESU_cell}, )
-Table
+#Table.sort_values(by=['LOAD', 'P'], ascending=True, inplace=True)
 
-fig = plt.figure(figsize=(10, 10))
-ax = Axes3D(fig)
+
+#%%
 
 x = Table.P
 y = Table.LOAD
 z = Table.U
+
 print(len(x))
+
+fig = plt.figure(figsize=(10, 10))
+ax = Axes3D(fig)
+
+ax.scatter(x, y, z, s=50)
+plt.xlabel('P', fontsize='30', labelpad=30)
+plt.ylabel('LOAD', fontsize='30', labelpad=30)
+ax.set_zlabel('U', fontsize='30', labelpad=30)
+plt.show()
+
+#px.scatter_3d(x=Table.P, y=Table.LOAD, z=Table.U)
+
+#%%
+
+import matplotlib.cm as cm
+
+Func_U = interpolate.interp2d(Table.P, Table.LOAD, Table.U, kind='cubic')
+
+
+#计算480*480的网格上的插值
+Table_test = pd.DataFrame({'LOAD': Table.LOAD, 'P': Table.P}, )
+print(Table_test.size)
+
+
+Table_test_2 = pd.DataFrame({'LOAD': Table.LOAD, 'P': Table.P}, )
+Table_New=Table_test.append(Table_test_2, ignore_index=True)
+Table_New.sort_values(by=[ 'P'], ascending=True, inplace=True)
+
+
+
+
+New_U = Func_U(Table_New.P, Table_New.LOAD)
+New_LOAD = Table_New.LOAD
+New_P = Table_New.P
+
+xnew = New_P
+ynew = New_LOAD
+znew = New_U
+
+fig = plt.figure(figsize=(10, 10))
+ax = Axes3D(fig)
 
 # ax.scatter(x, y, z, s=100)
 # plt.xlabel('P', fontsize='30', labelpad=30)
@@ -76,43 +119,20 @@ print(len(x))
 # ax.set_zlabel('U', fontsize='30', labelpad=30)
 # plt.show()
 
-
-
-
-#%%
-#Func_P = interpolate.interp1d(Table.P, Table.U, kind='kind')
-
-
-
-#%%
-
-Func_U = interpolate.interp2d(Table.P, Table.LOAD, Table.U, kind='cubic')
-
-
-New_U=Func_U(Table.P, Table.LOAD)
-New_LOAD=Table.LOAD
-New_P=Table.P
-
-print(len(New_U))
-print(len(New_P))
-print(len(New_LOAD))
-
-#%%
-
-import matplotlib.cm as cm
-
-x = New_P
-y = New_LOAD
-z = New_U
-
-ax.plot_surface(x,
-                y,
-                z,
-                rstride=2,
-                cstride=2,
-                cmap=cm.coolwarm,
-                linewidth=0.5,
-                antialiased=True)
+ax2 = plt.subplot(1, 2, 2, projection='3d')
+surf2 = ax2.plot_surface(xnew,
+                         ynew,
+                         znew,
+                         rstride=2,
+                         cstride=2,
+                         cmap=cm.coolwarm,
+                         linewidth=0.5,
+                         antialiased=True)
+ax2.set_xlabel('xnew')
+ax2.set_ylabel('ynew')
+ax2.set_zlabel('fnew(x, y)')
+#plt.colorbar(surf2, shrink=0.5, aspect=5)  #标注
 
 plt.show()
+
 # %%
